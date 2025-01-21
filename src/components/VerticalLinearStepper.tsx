db43e42014c242331d89d2c67bd215bcfdcb1ffe
Gonçalo <gteixei@uva.nl>
2025-01-21 18:43:48 +0100

    First steps added, still no content

---
+++ src/components/VerticalLinearStepper.tsx
@@ -0,0 +1,123 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Stepper from '@mui/material/Stepper';
+import Step from '@mui/material/Step';
+import StepLabel from '@mui/material/StepLabel';
+import StepContent from '@mui/material/StepContent';
+import Button from '@mui/material/Button';
+import Paper from '@mui/material/Paper';
+import Typography from '@mui/material/Typography';
+
+type Content = { label: string; hasButtons?: boolean };
+
+const steps: Array<Content> = [
+  {
+    label: 'Approach'
+  },
+  {
+    label: 'Fetch/compute',
+    hasButtons: false
+  },
+  {
+    label: 'Visualisation'
+  },
+  {
+    label: 'Deployment',
+    hasButtons: false
+  }
+];
+
+function StepOne() {
+  return <div>Step 1</div>;
+}
+
+function StepTwo() {
+  return <div>Step 2</div>;
+}
+
+function StepThree() {
+  return <div>Step 3</div>;
+}
+
+function StepFour() {
+  return <div>Step 4</div>;
+}
+
+function ContentHandler({ step }: { step: number }) {
+  switch (step) {
+    default:
+    case 0:
+      return <StepOne />;
+    case 1:
+      return <StepTwo />;
+    case 2:
+      return <StepThree />;
+    case 3:
+      return <StepFour />;
+  }
+}
+
+export default function VerticalLinearStepper() {
+  const [activeStep, setActiveStep] = React.useState(0);
+
+  const handleNext = () => {
+    setActiveStep(prevActiveStep => prevActiveStep + 1);
+  };
+
+  const handleBack = () => {
+    setActiveStep(prevActiveStep => prevActiveStep - 1);
+  };
+
+  const handleReset = () => {
+    setActiveStep(0);
+  };
+
+  return (
+    <Box sx={{ maxWidth: 400 }}>
+      <Stepper activeStep={activeStep} orientation="vertical">
+        {steps.map((step, index) => (
+          <Step key={step.label}>
+            <StepLabel
+              optional={
+                index === steps.length - 1 ? (
+                  <Typography variant="caption">Last step</Typography>
+                ) : null
+              }
+            >
+              {step.label}
+            </StepLabel>
+            <StepContent>
+              <ContentHandler step={activeStep} />
+              {step.hasButtons !== false && (
+                <Box sx={{ mb: 2 }}>
+                  <Button
+                    variant="contained"
+                    onClick={handleNext}
+                    sx={{ mt: 1, mr: 1 }}
+                  >
+                    {index === steps.length - 1 ? 'Finish' : 'Continue'}
+                  </Button>
+                  <Button
+                    disabled={index === 0}
+                    onClick={handleBack}
+                    sx={{ mt: 1, mr: 1 }}
+                  >
+                    Back
+                  </Button>
+                </Box>
+              )}
+            </StepContent>
+          </Step>
+        ))}
+      </Stepper>
+      {activeStep === steps.length && (
+        <Paper square elevation={0} sx={{ p: 3 }}>
+          <Typography>All steps completed - you&apos;re finished</Typography>
+          <Button onClick={handleReset} sx={{ mt: 1, mr: 1 }}>
+            Reset
+          </Button>
+        </Paper>
+      )}
+    </Box>
+  );
+}
--- src/widget.tsx
+++ src/widget.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { ReactWidget } from '@jupyterlab/apputils';
-import { Paper } from '@mui/material';
+import { Grid2, Paper } from '@mui/material';
 
 // import BandHighLight from './components/BandHighLight';
 // import ElementHighlights from './components/ElementHighlights';
-import MapComponent from './components/map/MapComponent';
+// import MapComponent from './components/map/MapComponent';
+import VerticalLinearStepper from './components/VerticalLinearStepper';
 
 const styles: Record<string, React.CSSProperties> = {
   main: {
@@ -20,26 +21,17 @@ const styles: Record<string, React.CSSProperties> = {
     display: 'flex',
     justifyContent: 'center',
     alignItems: 'center',
-    flex: '0 1 50%',
-    width: '50%'
-    // border: '1px solid green',
-    // boxSizing: 'border-box',
+    flex: '0 1 100%',
+    width: '100%'
   }
 };
 
-function GridContent({ index }: { index: number }) {
-  switch (index) {
-    // case 1:
-    //   return <BandHighLight />;
-    // case 2:
-    //   return <ElementHighlights />;
-    // case 3:
-    //   return <BandHighLight />;
-    case 1:
-      return <MapComponent />;
-    default:
-      return <span>{'Grid element ' + String(index)}</span>;
-  }
+function GridContent() {
+  return (
+    <Grid2>
+      <VerticalLinearStepper />
+    </Grid2>
+  );
 }
 
 /**
@@ -48,17 +40,11 @@ function GridContent({ index }: { index: number }) {
  * @returns The React component
  */
 const App = (): JSX.Element => {
-  const gridElements = Array.from(new Array(4)).map((_, index) => index + 1);
-
   return (
     <div style={styles.main}>
-      {gridElements.map(value => {
-        return (
-          <Paper key={`grid-element-${value}`} style={styles.grid}>
-            <GridContent index={value} />
-          </Paper>
-        );
-      })}
+      <Paper style={styles.grid}>
+        <GridContent />
+      </Paper>
     </div>
   );
 };
