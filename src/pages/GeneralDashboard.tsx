5818c97bf59f9f3eebf60b49e50de3ad6f1a5e4c
Gonçalo <goncalo.ferreira@student.uva.nl>
2025-04-03 16:57:17 +0200

    Added metrics from server into the welcome page

--- src/pages/GeneralDashboard.tsx
+++ src/pages/GeneralDashboard.tsx
@@ -3,7 +3,21 @@ import React from 'react';
 
 // import BandHighLight from '../components/BandHighLight';
 // import ElementHighlights from '../components/ElementHighlights';
-import MapComponent from '../components/map/MapComponent';
+// import MapComponent from '../components/map/MapComponent';
+
+/**
+ * This is something temporary. Just for the demo on April 8th, on All-Hands Meeting for GreenDIGIT.
+ */
+const chartLinks = [
+  // CPU Usage: used and total
+  'http://localhost:3000/d-solo/behmsglt2r08wa/2025-04-08-demo?orgId=1&from=1743669689152&to=1743691289152&timezone=browser&theme=light&panelId=2&__feature.dashboardSceneSolo',
+  // Memory used
+  'http://localhost:3000/d-solo/behmsglt2r08wa/2025-04-08-demo?orgId=1&from=1743669689152&to=1743691289152&timezone=browser&theme=light&panelId=3&__feature.dashboardSceneSolo',
+  // Network received/sent
+  'http://localhost:3000/d-solo/behmsglt2r08wa/2025-04-08-demo?orgId=1&from=1743669689152&to=1743691289152&timezone=browser&theme=light&panelId=4&__feature.dashboardSceneSolo',
+  // Thread Nr.
+  'http://localhost:3000/d-solo/behmsglt2r08wa/2025-04-08-demo?orgId=1&from=1743670340144&to=1743691940144&timezone=browser&theme=light&panelId=5&__feature.dashboardSceneSolo'
+];
 
 const styles: Record<string, React.CSSProperties> = {
   main: {
@@ -29,12 +43,13 @@ const styles: Record<string, React.CSSProperties> = {
 
 interface ITempIframe {
   keyIndex: number;
+  url: string;
 }
 
-function TempIframe({ keyIndex }: ITempIframe) {
+function TempIframe({ keyIndex, url }: ITempIframe) {
   return (
     <iframe
-      src="http://localhost:3000/d-solo/behmsglt2r08wa/memory-and-cpu?orgId=1&from=1743658807612&to=1743659109100&timezone=browser&theme=light&panelId=1&__feature.dashboardSceneSolo"
+      src={url}
       width="100%"
       height="400px"
       sandbox="allow-scripts allow-same-origin"
@@ -47,33 +62,34 @@ function TempIframe({ keyIndex }: ITempIframe) {
 
 export default function GeneralDashboard() {
   function GridContent({ index }: { index: number }) {
-    switch (index) {
-      case 1:
-        return <TempIframe keyIndex={1} />;
-      case 2:
-        return <TempIframe keyIndex={2} />;
-      case 3:
-        return <MapComponent />;
-      default:
-        return <span>{'Grid element ' + String(index)}</span>;
-    }
+    return <TempIframe keyIndex={index} url={chartLinks[index]} />;
+    // switch (index) {
+    //   case 1:
+    //     return <TempIframe keyIndex={1} />;
+    //   case 2:
+    //     return <TempIframe keyIndex={2} />;
+    //   case 3:
+    //     return <MapComponent />;
+    //   default:
+    //     return <span>{'Grid element ' + String(index)}</span>;
+    // }
   }
 
-  const gridElements = Array.from(new Array(3)).map((_, index) => index + 1);
+  const gridElements = Array.from(new Array(chartLinks.length));
 
   return (
     <div style={styles.main}>
-      {gridElements.map(value => {
+      {gridElements.map((value: number, index: number) => {
         return (
           <Paper
             key={`grid-element-${value}`}
             style={{
-              ...styles.grid,
-              minWidth: value === 3 ? '100%' : '50%',
-              flex: value === 3 ? '0 1 100%' : '0 1 50%'
+              ...styles.grid
+              // minWidth: value === 3 ? '100%' : '50%',
+              // flex: value === 3 ? '0 1 100%' : '0 1 50%'
             }}
           >
-            <GridContent index={value} />
+            <GridContent index={index} />
           </Paper>
         );
       })}
